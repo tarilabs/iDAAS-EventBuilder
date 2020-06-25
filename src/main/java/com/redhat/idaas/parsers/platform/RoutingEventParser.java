56b81733aa2ce9fc4413f2816188e1a0b813a4be
Alan Scott <alscott@redhat.com>
2020-06-25 09:04:26 -0500

    Updating EventBuilder, tweaking HL7 parsing and validation.

--- src/main/java/com/redhat/idaas/parsers/platform/RoutingEventParser.java
+++ src/main/java/com/redhat/idaas/parsers/platform/RoutingEventParser.java
@@ -20,7 +20,13 @@ public class RoutingEventParser {
 	public RoutingEvent buildRoutingEvent(String body) {
 		RoutingEvent routingEvent = new RoutingEvent();
 
-		String mshDelimiterDetails;
+		// Delimiters
+		String fieldDelimiter;
+		String componentDelimiter;
+		String subComponentsDelimiter;
+		String fieldRepeaterDelimiter;
+		String escapeDelimiter;
+		// MSH Segment Details
 		String[] mshSegmentDetails;
 		// Variables used within Parsing
 		String uMessageID = null;
@@ -32,25 +38,38 @@ public class RoutingEventParser {
 		for (String segmentData : messageSegments) {
 			if (segmentData.substring(0, 3).equals("MSH")) {
 				// Parse MSH and Populating Parsing Variables
-				mshDelimiterDetails = segmentData.substring(3, 4);
+				fieldDelimiter = segmentData.substring(3, 4);
+				componentDelimiter = segmentData.substring(4, 5);
+				fieldRepeaterDelimiter = segmentData.substring(6,7);
+				escapeDelimiter = segmentData.substring(7,8);
 				// Spilt MSH Segment
 				String msgSegment = segmentData;
-				String[] segmentDetails = msgSegment.split("["+mshDelimiterDetails+"]",0);
+				String[] segmentDetails = segmentData.split("["+fieldDelimiter+"]",0);
 				System.out.println("Segment Count"+segmentDetails.length);
+				// Splitting Routing Data
+				String messageTypeData = segmentDetails[8].toString();
+				//String[] messageTypeDetails = messageTypeData.split("["+componentDelimiter+"]");
+				// Splitting MessageTime
+				String messageDateTime = segmentDetails[6].toString();
+				String messageSendingDate = messageDateTime.substring(0,8);
+				String messageSendingHour = messageDateTime.toString().substring(8,10);
+				String messageSendingTime = messageDateTime.toString().substring(8,messageDateTime.length());
+				// Create UMID ID
+				String messageUMID = segmentDetails[3].toString()+"_"+segmentDetails[2].toString()+"_"+
+						messageTypeData.substring(0,3)+"_"+ segmentDetails[9].toString();
 				// Populate into Routing Event
-				routingEvent.setSendingApp("");
-				routingEvent.setFacilityId(segmentDetails[2].toString());
-				routingEvent.setMessageEvent("");
-				routingEvent.setMessageType("");
+				routingEvent.setSendingApp(segmentDetails[2].toString());
+				routingEvent.setFacilityId(segmentDetails[3].toString());
 				routingEvent.setIndustryStd("HL7v2");
 				routingEvent.setMessageDateTime(segmentDetails[6].toString());
-				routingEvent.setMessageDate("");
-				routingEvent.setMessageHour("");
-				routingEvent.setMessageTime("");
+				routingEvent.setMessageDate(messageSendingDate);
+				routingEvent.setMessageHour(messageSendingHour);
+				routingEvent.setMessageTime(messageSendingTime);
+				routingEvent.setMessageType(messageTypeData.substring(0,2));
+				routingEvent.setMessageEvent(messageTypeData.substring(4,6));
 				routingEvent.setMessageId(segmentDetails[9].toString());
-				routingEvent.setUniqueMessageId("");
+				routingEvent.setUniqueMessageId(messageUMID);
 				routingEvent.setMessageVersion(segmentDetails[11].toString());
-
 				routingEvent.setMessageData(body);
 			}
 			// end of for loop
--- src/main/java/com/redhat/idaas/parsers/platform/hl7ParsingDelimiters.java
+++ src/main/java/com/redhat/idaas/parsers/platform/hl7ParsingDelimiters.java
@@ -14,9 +14,7 @@ public class hl7ParsingDelimiters {
 	public static com.redhat.idaas.pojos.platform.hl7MessageDelimiters  buildDelimiters(String segmentData)
 	{
 		com.redhat.idaas.pojos.platform.hl7MessageDelimiters hl7Delimiters = new com.redhat.idaas.pojos.platform.hl7MessageDelimiters();
-		
-		
-		
+
 		return hl7Delimiters;
 	}
 }
