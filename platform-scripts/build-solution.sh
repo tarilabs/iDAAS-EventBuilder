ac9e4651406e05beb5b04c11eaa22d622056169c
Alan Scott <alscott@redhat.com>
2020-08-01 02:10:25 -0500

    Updated and tested scripts

--- platform-scripts/build-solution.sh
+++ platform-scripts/build-solution.sh
@@ -1,8 +1,14 @@
 # Change Directory to solution on local machine
-echo "iDAAS - Connect Clinical Middle Tier"
-cd /Users/alscott/Development/IntelliJ/OpenSource-iDAAS/iDAAS-EventBuilder/
+echo $PWD
+echo "iDAAS - Event Builder Distribution"
+cd $PWD
+cd ../
 
 /usr/local/bin/mvn clean install
 echo "Maven Build Completed"
 /usr/local/bin/mvn package
 echo "Maven Release Completed"
+cd target
+cp idaas-*.jar idaas-eventbuilder.jar
+echo "Copied Release Specific Version to General version"
+
--- platform-scripts/start-solution.sh
+++ platform-scripts/start-solution.sh
@@ -1,8 +1,6 @@
-./start_Kafka.sh &
-
 echo "The current working directory: $PWD"
-cd ..
-echo "The current working directory: $PWD"
+cd $PWD
+cd ../..
 cd target
 echo "The current working directory: $PWD"
-java -jar idaas-connect-clinical-industrystds-1.5.0-SNAPSHOT.jar
+java -jar idaas-eventbuilder.jar
