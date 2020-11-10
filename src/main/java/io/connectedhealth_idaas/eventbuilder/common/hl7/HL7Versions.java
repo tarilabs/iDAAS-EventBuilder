74a1cbceaefd7da4757b236a1d9958097c67a18d
Alan Scott <alscott@redhat.com>
2020-11-10 15:57:40 -0600

    Commits to help with Message Building and enhanced HL7Versions constants

--- src/main/java/io/connectedhealth_idaas/eventbuilder/common/hl7/HL7Versions.java
+++ src/main/java/io/connectedhealth_idaas/eventbuilder/common/hl7/HL7Versions.java
@@ -4,7 +4,12 @@ public enum HL7Versions {
     VERSION_2_5_1("2.5.1"),
     VERSION_2_6("2.6"),
     VERSION_2_7("2.7"),
-    VERSION_2_8("2.8");
+    VERSION_2_7_1("2.7.1"),
+    VERSION_2_8("2.8"),
+    VERSION_2_8_1("2.8.1"),
+    VERSION_2_8_2("2.8.2"),
+    VERSION_2_9("2.9")
+    ;
 
     private String value;
 
